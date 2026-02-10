f0890e31d032fcf7da36cecf6751ab1cce232a4c
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-02-10 11:15:49 +0000

    Condicoes ternarias

---
+++ par_ou_impar.py
@@ -0,0 +1,9 @@
+def par_ou_impar(n):
+    return 'par' if n % 2 == 0 else 'impar'
+
+
+print(par_ou_impar(6))
+print(par_ou_impar(3))
+print(par_ou_impar(100))
+print(par_ou_impar(91))
+print(par_ou_impar(57))
---
+++ positivo_negativo.py
@@ -0,0 +1,8 @@
+def sinal(n):
+    return f'{n} é positivo' if n > 0 else f'{n} é negativo'
+
+print(sinal(-1))
+print(sinal(3))
+print(sinal(-3))
+print(sinal(-4))
+print(sinal(10))
