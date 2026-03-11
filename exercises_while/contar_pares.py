2579e2f88f4821100db6169791083127ce6fb9e0
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-03-11 21:02:26 +0000

    Change for loop to while loop for even number collection
    Refactor to use a while loop for collecting even numbers.

--- exercises_while/contar_pares.py
+++ exercises_while/contar_pares.py
@@ -1,18 +1,31 @@
 num_pares = []
 while True:
+    '''
+    Peça ao usuário um número inteiro positivo.
+    Use while para mostrar todos os números pares de 0 até esse número.
+    '''
     try:
         numero = int(input('Digite um número: '))
         break
     except ValueError:
         print('Digite apenas números.')
-        
-for i in range(numero + 1):
+
+i = 0
+while i < numero + 1:
     if i % 2 == 0:
         if i > 0:
             num_pares.append(i)
-
+    i += 1
+        
+#for i in range(numero + 1):
+#    if i % 2 == 0:
+#       if i > 0:
+#            num_pares.append(i)
 
 print(num_pares)
+
+
+
 
 
 
