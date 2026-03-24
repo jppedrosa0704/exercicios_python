937d0e71d0acf07a4dc20371d78240a8e54f27f5
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-03-24 15:51:12 +0000

    Add files via upload
    Exercício: Criando uma Classe "Pessoa"
    
    Crie uma classe chamada Pessoa com as seguintes características:
    
    🔹 Atributos:
    nome
    idade
    🔹 Métodos:
    apresentar() → deve imprimir algo como:
    "Olá, meu nome é João e tenho 25 anos."
    
    🚀 Desafio extra (opcional):
    
    Se quiser praticar um pouco mais, adiciona:
    
    Um método fazer_aniversario() que aumenta a idade em +1
    Depois chama o apresentar() de novo pra ver a mudança

--- poo_exercicio_simples/criando_uma_classe_pessoa_basico.py
+++ poo_exercicio_simples/criando_uma_classe_pessoa_basico.py
@@ -1,1 +1,23 @@
+# Atributos:
+# nome
+# idade
+# 🔹 Métodos:
+# apresentar() → deve imprimir algo como:
+# "Olá, meu nome é João e tenho 25 anos."
 
+class pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self):
+        print(f'olá, meu nome é {self.nome} e tenho {self.idade} anos.')
+
+    def fazer_aniversario(self):
+        self.idade += 1
+
+p1 = pessoa('João', 25)
+p1.apresentar()
+
+p1.fazer_aniversario()
+p1.apresentar()
